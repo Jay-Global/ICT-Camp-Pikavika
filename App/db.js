cb6a714eb212ab4db704825912eef2f98d2791c2
Essi Terho <essi.j.terho@gmail.com>
2022-04-14 15:41:37 +0300

    Second send button deleted

--- App/db.js
+++ App/db.js
@@ -42,6 +42,43 @@ document.getElementById("button-send").onclick = () => {
   modalForm.style.display = "none";
   modalThankYou.style.display = "block";
   placeMarkerAndPanTo(currentLatLng, map);
+
+  //Send pictures
+  //checks if files are selected
+  if (files.length != 0) {
+    //Loops through all the selected files
+    for (let i = 0; i < files.length; i++) {
+      //create a storage reference
+      var storage = firebase.storage().ref(files[i].name);
+
+      //upload file
+      var upload = storage.put(files[i]);
+
+      //update progress bar
+      // Toimii, mutta jos tuon poistaa niin ei. En tiiä vaan miten sais näkymään
+
+      // upload.on(
+      //   "state_changed",
+      //   function progress(snapshot) {
+      //     var percentage =
+      //       (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      //     document.getElementById("progress").value = percentage;
+      //   },
+
+      //   function error() {
+      //     alert("error uploading file");
+      //   },
+
+      //   function complete() {
+      //     document.getElementById(
+      //       "uploading"
+      //     ).innerHTML += `${files[i].name} ="uploaded" <br />`;
+      //   }
+      // );
+    }
+  } else {
+    alert("No file chosen");
+  }
 };
 
 function queryFirebaseData() {
--- App/script.js
+++ App/script.js
@@ -74,50 +74,12 @@ buttonOpenForm.onclick = function () {
   modalForm.style.display = "block";
 };
 
-//Upload a picture
+//Upload pictures
 var files = [];
 document.getElementById("files").addEventListener("change", function (e) {
   files = e.target.files;
   for (let i = 0; i < files.length; i++) {
     console.log(files[i]);
-  }
-});
-
-document.getElementById("send").addEventListener("click", function () {
-  //checks if files are selected
-  if (files.length != 0) {
-    //Loops through all the selected files
-    for (let i = 0; i < files.length; i++) {
-      //create a storage reference
-      var storage = firebase.storage().ref(files[i].name);
-
-      //upload file
-      var upload = storage.put(files[i]);
-
-      //update progress bar
-      // Toimii, mutta jos tuon poistaa niin ei. En tiiä vaan miten sais näkymään
-
-      // upload.on(
-      //   "state_changed",
-      //   function progress(snapshot) {
-      //     var percentage =
-      //       (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      //     document.getElementById("progress").value = percentage;
-      //   },
-
-      //   function error() {
-      //     alert("error uploading file");
-      //   },
-
-      //   function complete() {
-      //     document.getElementById(
-      //       "uploading"
-      //     ).innerHTML += `${files[i].name} ="uploaded" <br />`;
-      //   }
-      // );
-    }
-  } else {
-    alert("No file chosen");
   }
 });
 
