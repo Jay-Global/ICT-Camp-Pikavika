9cc4376e10e16b197b72ae8b2bad3c85b9b8412c
Essi Terho <essi.j.terho@gmail.com>
2022-04-14 15:18:45 +0300

    Image upload added and fixed

--- App/script.js
+++ App/script.js
@@ -74,6 +74,68 @@ buttonOpenForm.onclick = function () {
   modalForm.style.display = "block";
 };
 
+//Upload a picture
+var files = [];
+document.getElementById("files").addEventListener("change", function (e) {
+  files = e.target.files;
+  for (let i = 0; i < files.length; i++) {
+    console.log(files[i]);
+  }
+});
+
+document.getElementById("send").addEventListener("click", function () {
+  //checks if files are selected
+  if (files.length != 0) {
+    //Loops through all the selected files
+    for (let i = 0; i < files.length; i++) {
+      //create a storage reference
+      var storage = firebase.storage().ref(files[i].name);
+
+      //upload file
+      var upload = storage.put(files[i]);
+
+      //update progress bar
+      // Toimii, mutta jos tuon poistaa niin ei. En tiiä vaan miten sais näkymään
+
+      // upload.on(
+      //   "state_changed",
+      //   function progress(snapshot) {
+      //     var percentage =
+      //       (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      //     document.getElementById("progress").value = percentage;
+      //   },
+
+      //   function error() {
+      //     alert("error uploading file");
+      //   },
+
+      //   function complete() {
+      //     document.getElementById(
+      //       "uploading"
+      //     ).innerHTML += `${files[i].name} ="uploaded" <br />`;
+      //   }
+      // );
+    }
+  } else {
+    alert("No file chosen");
+  }
+});
+
+function getFileUrl(filename) {
+  //create a storage reference
+  var storage = firebase.storage().ref(filename);
+
+  //get file url
+  storage
+    .getDownloadURL()
+    .then(function (url) {
+      console.log(url);
+    })
+    .catch(function (error) {
+      console.log("error encountered");
+    });
+}
+
 // document.getElementById("button-open-defects").onclick = () => {
 //   queryFirebaseData();
 // };
